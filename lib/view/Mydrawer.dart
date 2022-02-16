dfc50ccf8516337f48eda5517862d0a9315d8af2
Remy <remy@spliiit.com>
2022-02-16 09:08:51 +0100

    set fire base config

--- lib/view/Mydrawer.dart
+++ lib/view/Mydrawer.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
@@ -8,16 +7,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class myDrawer extends StatefulWidget{
+class myDrawer extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return myDrawerState();
   }
-
 }
 
-class myDrawerState extends State<myDrawer>{
+class myDrawerState extends State<myDrawer> {
   // Variable de la f
   String? identifiant;
   Users? utilisateur;
@@ -27,239 +25,233 @@ class myDrawerState extends State<myDrawer>{
   Uint8List? bytesImage;
   String imageFileName = "";
 
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    FirestoreHelper().getIdentifiant().then((value){
+    FirestoreHelper().getIdentifiant().then((value) {
       setState(() {
         identifiant = value;
       });
-      FirestoreHelper().getUser(identifiant!).then((value){
+      FirestoreHelper().getUser(identifiant!).then((value) {
         setState(() {
           utilisateur = value;
         });
       });
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    FirestoreHelper().getIdentifiant().then((value){
+    FirestoreHelper().getIdentifiant().then((value) {
       setState(() {
         identifiant = value;
       });
-      FirestoreHelper().getUser(identifiant!).then((value){
+      FirestoreHelper().getUser(identifiant!).then((value) {
         setState(() {
           utilisateur = value;
         });
       });
     });
     return Container(
-      padding: EdgeInsets.all(20),
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width/2,
-      color: Colors.white,
-      child:  Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          InkWell(
-            child: Container(
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: (utilisateur!.image == null)?NetworkImage("https://firebasestorage.googleapis.com/v0/b/firstprojetimm.appspot.com/o/image_disponible.png?alt=media&token=809cfa6c-b1af-44e1-bd85-a12ae9ef0f39"):NetworkImage(utilisateur!.image!),
-                    fit: BoxFit.fill
-
+        padding: EdgeInsets.all(20),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width / 2,
+        color: Colors.white,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            InkWell(
+              child: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: (utilisateur?.image == null)
+                          ? NetworkImage(
+                              "https://firebasestorage.googleapis.com/v0/b/firstprojetimm.appspot.com/o/image_disponible.png?alt=media&token=809cfa6c-b1af-44e1-bd85-a12ae9ef0f39")
+                          : NetworkImage(utilisateur!.image!),
+                      fit: BoxFit.fill),
                 ),
               ),
-            ),
-            onTap: (){
-              //appuye une fois cela affiche mon image en plus 400*400
-              (utilisateur!.image==null)?Container():printImage();
-
-            },
-            onLongPress: () async {
-              //appuyer longtemps ça doit récuperer une image sur mon téléphone
-              FilePickerResult? resultat = await FilePicker.platform.pickFiles(
-                withData: true,
-                type: FileType.image
-              );
-              if(resultat!=null){
-                setState(() {
-                  imageFileName = resultat.files.first.name;
-                  bytesImage = resultat.files.first.bytes;
-                  print(imageFileName);
-                });
-                await afficherImage();
-              }
-
-
-
-            },
-          ),
-          //Avatar
-
-          SizedBox(height: 10,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("${utilisateur?.pseudo}",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-              IconButton(
-                  onPressed: (){
-                        UpdateBox();
-                  },
-                  icon: Icon(Icons.edit)
-              )
-            ],
-          ),
-
-          SizedBox(height: 10,),
-          Row(
-            children: [
-              Icon(Icons.mail),
-              Text("${utilisateur?.mail}"),
-            ],
-          ),
-          SizedBox(height: 10,),
-          Text("${utilisateur!.prenom}  ${utilisateur!.nom}"),
-
-          SizedBox(height: 10,),
-          (utilisateur!.dateNaissance== null)?Text(dateFormat.format(time)):Text(dateFormat.format(utilisateur!.dateNaissance!)),
-
-
-
-
-
-          IconButton(
-              onPressed: (){
-                FirestoreHelper().logUser();
-                //chemein vers page principal
+              onTap: () {
+                //appuye une fois cela affiche mon image en plus 400*400
+                (utilisateur!.image == null) ? Container() : printImage();
               },
-              icon: Icon(Icons.exit_to_app_rounded,)
-          ),
-         IconButton(
-             onPressed: (){
-               //Création d'un boite de dialogue
-               BoxDelete();
+              onLongPress: () async {
+                //appuyer longtemps ça doit récuperer une image sur mon téléphone
+                FilePickerResult? resultat = await FilePicker.platform
+                    .pickFiles(withData: true, type: FileType.image);
+                if (resultat != null) {
+                  setState(() {
+                    imageFileName = resultat.files.first.name;
+                    bytesImage = resultat.files.first.bytes;
+                    print(imageFileName);
+                  });
+                  await afficherImage();
+                }
+              },
+            ),
+            //Avatar
 
-             },
-             icon: Icon(Icons.logout,color: Colors.red,)
-         ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "${utilisateur?.pseudo}",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                    onPressed: () {
+                      UpdateBox();
+                    },
+                    icon: Icon(Icons.edit))
+              ],
+            ),
 
-      )
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Icon(Icons.mail),
+                Text("${utilisateur?.mail}"),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("${utilisateur?.prenom}  ${utilisateur?.nom}"),
 
-    );
+            SizedBox(
+              height: 10,
+            ),
+            (utilisateur?.dateNaissance == null)
+                ? Text(dateFormat.format(time))
+                : Text(dateFormat.format(utilisateur!.dateNaissance!)),
+
+            IconButton(
+                onPressed: () {
+                  FirestoreHelper().logUser();
+                  //chemein vers page principal
+                },
+                icon: Icon(
+                  Icons.exit_to_app_rounded,
+                )),
+            IconButton(
+                onPressed: () {
+                  //Création d'un boite de dialogue
+                  BoxDelete();
+                },
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.red,
+                )),
+          ],
+        ));
   }
 
-
-  afficherImage(){
+  afficherImage() {
     return showDialog(
         barrierDismissible: false,
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return AlertDialog(
             title: Text("Souhatez-vous enregistrer cette image ? "),
-            content: Image.memory(bytesImage!,width: 400,height: 400,),
+            content: Image.memory(
+              bytesImage!,
+              width: 400,
+              height: 400,
+            ),
             actions: [
               ElevatedButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("Annuler")
-              ),
+                  child: Text("Annuler")),
               ElevatedButton(
-                  onPressed: (){
-                    FirestoreHelper().stockageImage(imageFileName, bytesImage!).then((value){
+                  onPressed: () {
+                    FirestoreHelper()
+                        .stockageImage(imageFileName, bytesImage!)
+                        .then((value) {
                       setState(() {
                         imageFilePath = value;
                       });
-                      Map<String,dynamic> map = {
+                      Map<String, dynamic> map = {
                         "IMAGE": imageFilePath,
                       };
                       FirestoreHelper().updateUser(utilisateur!.id, map);
                     });
                     Navigator.pop(context);
-
                   },
-                  child: Text("Enregistrer")
-              ),
+                  child: Text("Enregistrer")),
             ],
           );
-
-        }
-    );
+        });
   }
 
-
- printImage(){
+  printImage() {
     return showDialog(
-      barrierDismissible: false,
+        barrierDismissible: false,
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return AlertDialog(
-            content: Image.network(utilisateur!.image!,width: 400,height: 400,),
+            content: Image.network(
+              utilisateur!.image!,
+              width: 400,
+              height: 400,
+            ),
             actions: [
               ElevatedButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("Ok")
-              )
+                  child: Text("Ok"))
             ],
           );
+        });
+  }
 
-        }
-    );
-
- }
-
-UpdateBox(){
+  UpdateBox() {
     String update = "";
     return showDialog(
-      barrierDismissible: false,
+        barrierDismissible: false,
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return AlertDialog(
             title: Text("Modification"),
             content: TextField(
-              onChanged: (newValue){
+              onChanged: (newValue) {
                 setState(() {
                   update = newValue;
                 });
               },
-
             ),
             actions: [
               ElevatedButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("Annuler")
-              ),
+                  child: Text("Annuler")),
               ElevatedButton(
-                  onPressed: (){
-                    Map<String,dynamic> map = {
-                      "PSEUDO":update
-                    };
+                  onPressed: () {
+                    Map<String, dynamic> map = {"PSEUDO": update};
                     FirestoreHelper().updateUser(utilisateur!.id, map);
                     Navigator.pop(context);
                   },
-                  child: Text("Enregistrer")
-              )
+                  child: Text("Enregistrer"))
             ],
           );
-        }
-    );
-}
+        });
+  }
+
   //Création de la boite de la dialogue
-BoxDelete(){
+  BoxDelete() {
     return showDialog(
         context: context,
         barrierDismissible: false,
@@ -272,19 +264,11 @@ BoxDelete(){
                     onPressed: () {
                       Navigator.pop(context);
                     },
-                    child: Text("Non")
-                ),
-                ElevatedButton(
-                    onPressed: () {
-
-
-                    },
-                    child: Text("Oui")
-                ),
+                    child: Text("Non")),
+                ElevatedButton(onPressed: () {}, child: Text("Oui")),
               ],
             );
-          }
-          else {
+          } else {
             return AlertDialog(
               title: Text("Suppression du compte définitif ?"),
               actions: [
@@ -292,20 +276,11 @@ BoxDelete(){
                     onPressed: () {
                       Navigator.pop(context);
                     },
-                    child: Text("Non")
-                ),
-                ElevatedButton(
-                    onPressed: () {
-
-                    },
-                    child: Text("Oui")
-                ),
+                    child: Text("Non")),
+                ElevatedButton(onPressed: () {}, child: Text("Oui")),
               ],
             );
           }
-        }
-    );
-}
-
-
+        });
+  }
 }
